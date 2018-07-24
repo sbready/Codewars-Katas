c70961bdeba86a537e9f4b830b13ffaa464f3c73
Steven Bready <stevenbready@gmail.com>
2018-07-23 21:55:34 -0600

    last aod

---
+++ 8 kyu/Simple Multiplication/solution.js
@@ -0,0 +1,9 @@
+function simpleMultiplication(number) {
+    let answer
+    if ( number % 2 === 0 ) {
+      answer = number * 8
+    }else { 
+      answer = number * 9
+    }
+    return answer
+}
